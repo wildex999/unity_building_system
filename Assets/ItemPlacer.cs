a8fb53c4f562e58a14d4f82051f5b63b978e3e61
wildex999 <wildex999@gmail.com>
2015-03-13 00:58:23 +0100

    - Started work on an House Building engine =D - Testingsome stuff with Raycasting and the returned values. - Not sure which Unity files may be ignored, so including everything.

---
+++ Assets/ItemPlacer.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ItemPlacer : MonoBehaviour
+{
+	private Object itemInHand;
+	public float placeDistance = 5f;
+
+	// Use this for initialization
+	void Start ()
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		Camera cam = GetComponentInChildren<Camera> ();
+		Ray ray = cam.ViewportPointToRay (new Vector3 (0.5f, 0.5f, 0));
+		RaycastHit hit;
+		if (Physics.Raycast (ray, out hit, placeDistance)) {
+			Debug.DrawLine (transform.position, hit.point, Color.black, 0);
+			Debug.DrawRay (hit.point, hit.normal, Color.black, 0);
+		}
+	}
+}
